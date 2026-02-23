2f659e6f873f3a01f130497d0f8682ab0baabb59
Zac Garby <me@zacgarby.co.uk>
2026-02-23 12:47:01 +0000

    stuff

--- Calculator/Basic.lean
+++ Calculator/Basic.lean
@@ -2,17 +2,48 @@ import Calculator.Calculator
 
 open Tactic.Calculation
 
+set_option linter.style.multiGoal false
+
 def rev {a} : List a → List a
   | [] => []
   | x :: xs => rev xs ++ [x]
 
-structure RevSpec {a} : Type where
+def test {a} :
+  Σ' app : List a -> List a -> List a -> List a,
+  ∀ xs ys zs, app xs ys zs = xs ++ ys ++ zs := by
+  calculate fst
+  intro xs
+  induction xs <;> intro ys zs
+  case nil =>
+    rewrite [List.nil_append]
+    rfl
+  case cons x xs ih =>
+    dsimp
+    rewrite [<- ih]
+    by_def fst
+    generalize fst xs = h
+    rfl
+
+#print test
+#eval test.fst [1, 2] [3, 4] [5, 6]
+
+structure RevSpec a : Type where
   aux : List a -> List a -> List a
   correct : ∀ xs ys, aux xs ys = rev xs ++ ys
 
-structure RevSpec' : Type where
-  aux : List Int -> List Int -> List Int
-  correct : ∀ xs ys, aux xs ys = rev xs ++ ys
+def correct {a} : RevSpec a := by
+  calculate aux
+  intro xs
+  induction xs <;> intro ys
+  case nil =>
+    define aux.nil ys := ys
+  case cons x xs ih =>
+    rw [rev]
+    rw [List.append_assoc]
+    define aux.cons x xs aux_xs ys := aux_xs (x :: ys)
+    by_def aux
+    rw [<- ih]
+    rw [List.cons_append, List.nil_append]
 
 inductive Exp : Type
   | val : Nat -> Exp
@@ -38,6 +69,25 @@ structure CompSpec where
   comp : Exp -> Code -> Code
   exec : Code -> Stack -> Stack
   correct : ∀ e c s, exec c (eval e :: s) = exec (comp e c) s
+
+def comp_calc2 : CompSpec := by
+  calculate comp, exec
+  intro e
+  induction e <;> intros c s
+  case val n =>
+    rewrite [eval]
+    define exec.push n c' exec_c' s := exec_c' (n :: s)
+    define comp.val n c := .push n c
+  case add x y ih_x ih_y =>
+    rw [eval]
+    generalize h : eval x = u at ih_x
+    generalize j : eval y = v at ih_y
+    define exec.add c' exec_c' s := match s with
+          | m :: n :: s' => exec_c' ((n + m) :: s')
+          | _ => exec_c' s
+    define comp.add x y cx cy c := cx (cy (.add c))
+    by_def comp
+
 
 def comp_calc : CompSpec := by
   calculate comp, exec
--- Calculator/Calculator.lean
+++ Calculator/Calculator.lean
@@ -13,8 +13,36 @@ set_option linter.style.multiGoal false
 set_option linter.style.longLine false
 set_option linter.hashCommand false
 
-elab (name := defineTactic) "define" v:ident args:ident* ":=" to_term:term : tactic => do
-  let bind_name := v.getId
+def refold_def (fn within : Expr) : MetaM Expr := do
+  Meta.transform within
+    fun e => do
+      let fn_args := (<- whnf fn).getAppArgs
+      let rem := e.getAppNumArgs - fn_args.size
+      let bs := e.getBoundedAppArgs rem
+      let e_fn := e.getBoundedAppFn rem
+      if <- isDefEq e_fn (<- whnf fn) then
+        return .done (mkAppN fn bs)
+      return .continue
+
+-- finds 'name' in the local LCtx, so make sure you're in context
+def unroll_def (name : Name) (target : Expr) : MetaM Expr := do
+  let ctx <- getLCtx
+  let (some decl) := ctx.findFromUserName? name | throwError m!"no assumption with name: {name}"
+  let search_fn := decl.toExpr
+  Meta.transform target fun e => do
+    if e.getAppFn' != search_fn then return .continue
+    let e' <- whnf e
+    let back <- refold_def search_fn e'
+    return .done back
+
+elab (name := byDefTactic) "unroll" v:ident : tactic => Tactic.withMainContext do
+  let target <- Tactic.getMainTarget
+  let new <- unroll_def v.getId target
+  let goal <- Tactic.getMainGoal
+  let new_goal <- goal.replaceTargetDefEq new
+  Tactic.replaceMainGoal [new_goal]
+
+def define_clause (bind_name : Name) (args : TSyntaxArray `ident) (to_term : TSyntax `term) : Tactic.TacticM Unit := do
   let body_node: TSyntax `term <- `(fun $args* => $to_term)
   let mctx <- getMCtx
   match mctx.findUserName? bind_name with
@@ -28,7 +56,51 @@ elab (name := defineTactic) "define" v:ident args:ident* ":=" to_term:term : tac
           throwError m!"cannot re-define {<- mv.getTag} as {to_expr}\n  (already assigned to {Expr.mvar mv})"
       else
         mv.assignIfDefEq to_expr
+
+elab (name := defineTactic) "define" mod:("only")? v:ident args:ident* " := " to_term:term : tactic => do
+  define_clause v.getId args to_term
+  if !mod.isSome then
+    Tactic.withMainContext do
       Tactic.evalTactic (<- `(tactic| try rfl))
+
+elab "define'" p:term ":=" to_term:term : tactic => do
+  match p with
+  | `($f:ident $pat:term $rest*) => do
+    let pat <- liftMacroM <| expandMacros pat
+    let (con_stx, named, con_args_stx, ell) <- Term.expandApp pat
+    if !named.isEmpty || ell then throwUnsupportedSyntax
+    let con <- match con_stx with
+      | `($fId:ident)  => pure fId
+      | _ => throwErrorAt pat "expected a constructor application, but got {indentD pat}"
+    let some (Expr.const (.str _ con_name) _) <- resolveId? con "pattern"
+      | throwError "expected {con} to be a constructor"
+    let clause_name := Name.str f.getId con_name
+    let con_args <- con_args_stx.mapM fun
+      | .stx s => if s.isIdent then pure s else throwErrorAt s "expected an identifier"
+      | _ => unreachable!
+    for rv in rest do if !rv.raw.isIdent then throwErrorAt rv "expected an identifier"
+    let body_args := TSyntaxArray.mk (con_args ++ rest)
+    let body_node: TSyntax `term <- `(fun $body_args* => $to_term)
+    dbg_trace f!"con: {con_name}
+    args: {con_args_stx}
+    clause = {clause_name}
+    f = {f.getId}
+    rest: {rest}"
+    let s <- saveState
+    let main <- Tactic.getMainGoal
+    main.withContext do
+      let search_fn <- elabTerm f none
+      let fn <- elabTerm body_node none
+      let fn_ty <- inferType fn
+      _ <- Meta.transform fn (fun e => do
+        if <- isDefEq e.getAppFn search_fn then
+          dbg_trace f!"search {<- ppExpr search_fn} in sub: {<- ppExpr e}"
+        return .continue)
+      let (ms, bs, r) <- forallMetaTelescopeReducing fn_ty (some 1)
+      logInfo m!"got {ms} to {r}"
+    s.restore
+    return ()
+  | _ => throwUnsupportedSyntax
 
 #allow_unused_tactic! defineTactic
 
@@ -70,6 +142,66 @@ def match_struct_fields (goal_type : Expr) : TermElabM (Array Expr × Array (Nam
         throwError "oops, somehow constructed the wrong structure type {<- ppExpr type}"
       return (params, field_mvars)
 
+def intro_let_in_main_goal (name : Name) (ty val : Expr) (isDef : Bool := true)
+  : Tactic.TacticM FVarId := do
+  let mut main_mv <- Tactic.getMainGoal
+  if isDef then
+    main_mv <- main_mv.define name ty val
+  else
+    main_mv <- main_mv.assert name ty val
+  let (fv, new_main) <- main_mv.intro1P
+  Tactic.replaceMainGoal [new_main]
+  return fv
+
+def calc_intro_for (field_name : Name) (fields : Array (Name × Expr))
+    : Tactic.TacticM (FVarId × List (Name × Expr))
+  := do
+  let (field_ty, inp_ty, mot_ty) <- match fields.find? (fun (n, _) => n = field_name) with
+    | none => throwUnknownNameWithSuggestions field_name (extraMsg := m!", could be any of {fields.map (·.fst)}")
+    | some (_, field) => do
+      let field_type <- inferType field >>= instantiateMVars
+      match field_type.arrow? with
+      | none => throwError m!"cannot calculate non-arrow-type {field_type} of '{field_name}'"
+      | some t => pure (field_type, t)
+  -- find the recursor
+  let (inp_ty_name, ctors) <- of_inductive_ty inp_ty
+  let recursor <- mkConstWithFreshMVarLevels (.str inp_ty_name "rec")
+  let rec_ty <- inferType recursor
+  -- make the motive (a const function)
+  let motive_stx <- `(fun x => $(<- mot_ty.toSyntax))
+  let motive <- elabTerm motive_stx none
+  -- instantiate the recursor's implicit parameters to new mvars
+  -- until we get to the motive. then assign this to our motive, defined above
+  let mut (ms, bs, r) <- forallMetaTelescopeReducing rec_ty (some 1)
+  while bs.back!.isImplicit do
+    let mv := ms.back!.mvarId!
+    let (ms', bs', r') <- forallMetaTelescopeReducing r (some 1)
+    unless ms'.size == 1 do
+      throwError f!"wrong type!"
+    if (<- mv.getTag).eqStr "motive" then
+      mv.assignIfDefEq motive
+    if bs'.back!.isExplicit then break
+    (ms, bs, r) := (ms ++ ms', bs ++ bs', r')
+  -- get explicit_rec_ty, the recursor type with only explicit (algebra) arguments remaining.
+  -- make new mvars for these algebras, and give them useful usernames
+  let explicit_rec_ty <- instantiateExprMVars r
+  let (algebra_mv_exps, _, concl_ty) <- forallMetaTelescopeReducing explicit_rec_ty (some ctors.length)
+  let algebras := zip algebra_mv_exps.toList ctors
+  for (exp, ctor, _) in algebras do
+    let mv := exp.mvarId!
+    let username := ctor.updatePrefix field_name
+    mv.setUserName username
+    -- we also intro each recursor algebra as a local hypothesis in the main goal
+    _ <- intro_let_in_main_goal username (<- mv.getType) (.mvar mv)
+    Tactic.appendGoals [mv]
+  if !(<- isDefEq concl_ty field_ty) then
+    throwError m!"that's weird, the recursor's conclusion is the wrong type\n{concl_ty}\n  vs\n{field_ty}"
+  let final_ty <- instantiateExprMVars concl_ty
+  -- add a 'let ... = ...' for this constructor to the main goal, and then intro it as a hypothesis
+  let field_body := mkAppN recursor (ms ++ algebra_mv_exps)
+  let fv <- intro_let_in_main_goal field_name final_ty field_body
+  return (fv, algebras.map fun (mv, n, _) => (n, mv))
+
 elab (name := calculateTactic) "calculate " vs:ident,* : tactic => Tactic.withMainContext do
   -- look at main goal, get its fields
   let main_goal <- Tactic.getMainGoal
@@ -80,112 +212,72 @@ elab (name := calculateTactic) "calculate " vs:ident,* : tactic => Tactic.withMa
   -- for each ident 'v' listed:
   let fvs <- vs.getElems.mapM fun s => do
     let field_name := s.getId
-    -- find the constructor they're asking for, split into input and motive
-    let (field_ty, inp_ty, mot_ty) <- match spec_fields.find? (fun (n, _) => n = field_name) with
-      | none => throwUnknownNameWithSuggestions field_name
-      | some (_, field) => do
-        let field_type <- inferType field
-        match field_type.arrow? with
-        | none => throwError m!"cannot calculate non-arrow-type of '{field_name}'"
-        | some t => pure (field_type, t)
-    -- find the recursor
-    let (inp_ty_name, ctors) <- of_inductive_ty inp_ty
-    let recursor <- mkConstWithFreshMVarLevels (.str inp_ty_name "rec")
-    let rec_ty <- inferType recursor
-    -- make the motive (a const function)
-    let motive_stx <- `(fun x => $(<- mot_ty.toSyntax))
-    let motive <- elabTerm motive_stx none
-    -- instantiate the recursor's implicit parameters to new mvars
-    -- until we get to the motive. then assign this to our motive, defined above
-    let mut (ms, bs, r) <- forallMetaTelescopeReducing rec_ty (some 1)
-    while bs.back!.isImplicit do
-      let mv := ms.back!.mvarId!
-      let (ms', bs', r') <- forallMetaTelescopeReducing r (some 1)
-      unless ms'.size == 1 do
-        throwError f!"wrong type!"
-      if (<- mv.getTag).eqStr "motive" then
-        mv.assignIfDefEq motive
-      if bs'.back!.isExplicit then break
-      (ms, bs, r) := (ms ++ ms', bs ++ bs', r')
-    -- get explicit_rec_ty, the recursor type with only explicit (algebra) arguments remaining.
-    -- make new mvars for these algebras, and give them useful usernames
-    let explicit_rec_ty <- instantiateExprMVars r
-    let (algebra_mv_exps, _, concl_ty) <- forallMetaTelescopeReducing explicit_rec_ty (some ctors.length)
-    for (exp, ctor, _) in zip algebra_mv_exps.toList ctors do
-      let mv := exp.mvarId!
-      mv.setUserName (ctor.updatePrefix field_name)
-      Tactic.appendGoals [mv]
-    if !(<- isDefEq concl_ty field_ty) then
-      throwError m!"that's weird, the recursor's conclusion is the wrong type\n{concl_ty}\n  vs\n{field_ty}"
-    let final_ty <- instantiateExprMVars concl_ty
-    -- add a 'let ... = ...' for this constructor to the main goal, and then intro it as a hypothesis
-    let field_body := mkAppN recursor (ms ++ algebra_mv_exps)
-    let mut main_mv <- Tactic.getMainGoal
-    main_mv <- main_mv.define field_name final_ty field_body
-    let (fv, new_main) <- main_mv.intro1P
-    Tactic.replaceMainGoal [new_main]
-    return (field_name, fv)
+    let (fv, algebras) <- calc_intro_for field_name spec_fields
+    return (field_name, fv, algebras)
   -- split the main goal into its constructor fields, and set each one to the corresponding
   -- recursor binding from above
   let main_mv <- Tactic.getMainGoal
-  let new_mvs <- main_mv.constructor
-  Tactic.pushGoals new_mvs
-  fvs.forM fun (field_name, fv) => do
-    new_mvs.forM fun mv => do
-      if (<- mv.getTag) == field_name then
-        mv.assign (.fvar fv)
+  let field_mvs <- main_mv.constructor
+  Tactic.pushGoals field_mvs
+  for (field_name, fv, _) in fvs do
+    for field_mv in field_mvs do
+      if (<- field_mv.getTag) == field_name then
+        field_mv.assign (.fvar fv)
+        let t <- inferType (.mvar field_mv)
+        let (forall_args, _, _) <- forallMetaTelescopeReducing t
+        field_mv.withContext do
+          let lhs := mkAppN (.mvar field_mv) forall_args
+          let rhs <- reduce lhs
+          let mut eq <- mkEq lhs rhs
+          eq <- mkForallFVars forall_args eq
+          let mut proof <- mkEqRefl lhs
+          proof <- mkLambdaFVars forall_args proof
+          _ <- intro_let_in_main_goal (.str field_name "eq_def") eq proof (isDef := false)
 
-def rev {a} : List a → List a
-  | [] => []
-  | x :: xs => rev xs ++ [x]
+-- def rev {a} : List a → List a
+--   | [] => []
+--   | x :: xs => rev xs ++ [x]
 
-structure RevSpec (a : Type) : Type where
-  aux : List a -> List a -> List a
-  correct : ∀ xs ys, aux xs ys = rev xs ++ ys
+-- def test {a} :
+--   Σ' aux : List a -> List a -> List a,
+--   ∀ xs ys, aux xs ys = rev xs ++ ys := by
+--   calculate fst
+--   intro xs
+--   induction xs <;> intro ys
+--   case nil =>
+--     rewrite [rev, List.nil_append]
+--     -- define fst.cons x xs ih ys := fst
+--     define' fst (x::xs) ys := fst xs (x :: ys)
+--     -- define' fst (x::xs) foo bar := ys
+--     -- unroll fst
+--     -- rfl
+--   case cons x xs ih =>
+--     rewrite [rev]
+--     rewrite [List.append_assoc]
+--     rewrite [List.cons_append, List.nil_append]
+--     rewrite [<- ih]
+--     unroll fst
+--     generalize fst xs = h
+--     rfl
 
-def correct {a} : RevSpec a := by
-  calculate aux
-  intro xs
-  induction xs
-  case nil =>
-    intro ys
-    dsimp [rev]
-    define aux.nil ys := ys
-  case cons x xs ih =>
-    intro ys
-    dsimp [rev]
-    rw [List.append_assoc]
-    define aux.cons x xs aux_xs ys := aux_xs (x :: ys)
-    dsimp
-    rw [ih]
-
-inductive Exp : Type
-  | val : Nat -> Exp
-  | add : Exp -> Exp -> Exp
-  deriving BEq
-
-compile_inductive% Exp
-
-def eval : Exp -> Nat
-  | .val n => n
-  | .add x y => eval x + eval y
-
-inductive Code where
-  | push : Nat -> Code -> Code
-  | add : Code -> Code
-  | halt : Code
-
-compile_inductive% Code
-
-abbrev Stack := List Nat
-
-structure CompSpec where
-  comp : Exp -> Code -> Code
-  exec : Code -> Stack -> Stack
-  correct : ∀ e c s, exec c (eval e :: s) = exec (comp e c) s
-
--- def comp_calc : CompSpec := by
---   calculate comp, exec
+def test2 {a} :
+  Σ' len : List a -> Nat,
+  len [] = 0 ∧ ∀ xs x, len (x :: xs) = len xs + 1 := by
+  calculate fst
+  constructor
+  · define fst.nil := 0
+  · intro xs
+    induction xs
+    case cons y ys ih =>
+      intro x
+      rw [ih]
+      -- unroll fst
+      -- generalize fst ys + 1 = h
+      -- define' fst (x::xs) := fst xs + 1
+      define fst.cons x xs l_xs := l_xs + 1
+    case nil =>
+      intro x
+      dsimp [fst]
 
 end Calculation
 end Tactic
